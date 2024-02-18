ce97fccfb5741d82d6d5cb797f58582241370143
Econ Account <ouecon005@schooner1.oscer.ou.edu>
2024-02-18 16:43:38 -0600

    Completed PS4

--- ProblemSets/PS4/PS4a_Dong.R
+++ ProblemSets/PS4/PS4a_Dong.R
@@ -1,8 +1,13 @@
 library(jsonlite)
 library(tidyverse)
 
-getwd()
-setwd("/Users/boweidong/Dropbox/My Mac (Boweis-MacBook-Air.local)/Desktop/DScourseS24")
+url <- "https://www.vizgr.org/historical-events/search.php?format=json&begin_date=00000101&end_date=20240209&lang=en"
+destination_file <- "downloaded_file.json"
+
+download.file(url, destfile = destination_file, method = "auto")
+
+#getwd()
+#setwd("/Users/boweidong/Dropbox/My Mac (Boweis-MacBook-Air.local)/Desktop/DScourseS24")
 
 mylist <- fromJSON("dates.json")
 mydf <- bind_rows(mylist$result[-1])
